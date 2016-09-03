34238994a12f7c7dc6a3dda70421b73c357b6915
Jonathan Hockman <janathan.hockman@gmail.com>
2016-09-03 01:32:49 -0500

    Just a quick add to send a message to self to test parsing at start

--- lib/ex_zigbee.ex
+++ lib/ex_zigbee.ex
@@ -15,8 +15,13 @@ defmodule ExZigbee do
     opts = [strategy: :one_for_one, name: ExZigbee.Supervisor]
     {:ok, sup_pid} = Supervisor.start_link(children, opts)
 
+    # Send one to self
+    dest_addy = ExZigbee.Address.new(0x05, {0x01, 0x04}, {0x04, 0x02}, {0x0, 0x13, 0xA2, 0x0, 0x40, 0x99, 0x93, 0x58})
+    ExZigbee.SerialWorker.send(dest_addy, ExZigbee.Helpers.String.get_codepoints("Just a test message to self."))
+
+    # Send one to external device
     dest_addy = ExZigbee.Address.new(0x01, {0x01, 0x04}, {0x04, 0x02}, {0x0, 0x13, 0xA2, 0x0, 0x40, 0xE6, 0x5A, 0x6D})
-    ExZigbee.SerialWorker.send(dest_addy, ExZigbee.Helpers.String.get_codepoints("Just a test message"))
+    ExZigbee.SerialWorker.send(dest_addy, ExZigbee.Helpers.String.get_codepoints("Just a test message to external."))
 
     {:ok, sup_pid}
   end
