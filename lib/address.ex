a75a281fd41fda7bfa42e96eecc029fa2a3526f6
Jonathan Hockman <janathan.hockman@gmail.com>
2016-09-04 02:15:45 -0500

    Update readme. Update comments in ExZigbee.Address

--- lib/address.ex
+++ lib/address.ex
@@ -1,9 +1,9 @@
 defmodule ExZigbee.Address do
-  defstruct endpoint: nil,
-            profile: nil,
-            cluster: nil,
-            extended_address: nil,
-            short_address: nil
+  defstruct endpoint: nil, # integer
+            profile: nil, # tuple ex. {0x01, 0x04} for home automation
+            cluster: nil, # tuple ex. {0x02, 0x04} for thermostat
+            extended_address: nil, # tuple ex. {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff}
+            short_address: nil # tuple ex. {0xff, 0xfe}
 
   def new(endpoint, profile, cluster, extended_address, short_address) when is_integer(endpoint) and is_tuple(profile) and is_tuple(cluster) and is_tuple(extended_address) and is_tuple(short_address) do
     %__MODULE__{endpoint: endpoint, profile: profile, cluster: cluster, extended_address: extended_address, short_address: short_address}
